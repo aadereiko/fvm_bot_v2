27d3bb0bf01be6651da7196e057e1d4d192408d4
Alex <aadereiko@gmail.com>
2021-05-10 11:38:27 +0300

    change bot token

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from telegram import Bot
 
-fvm_token = "1369098570:AAEGEKqSjXgHXrX3Gkk0_YmV0QOnuiHEtko"
+fvm_token = "1824615819:AAFu0XQWQJHDgYu2cYt7SH1tKdPlzCZUxF4"
 marika_id = "399887481"
 ader_id = "323428638"
 sasha_id = "246505267"
